36a1a875ec37295ca73fe3d58bbd9c5aaacdf4a3
Oumuv <802711luo>
2018-03-06 17:30:09 +0800

    2018.3.6   first  commit

---
+++ src/main/java/com/oumuv/CongigClientApplication.java
@@ -0,0 +1,28 @@
+package com.oumuv;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+@RefreshScope
+public class CongigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping("/hi")
+	public String hi() {
+		return foo;
+	}
+}
